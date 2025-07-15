38b3a759da7715d447ecc8ce8ebf4044dee85aef
Wayne <waynekaila1@gmail.com>
2025-07-15 12:00:08 +0100

    start

---
+++ 01/01_exercice01.py
@@ -0,0 +1,9 @@
+prenom = input('Entrez votre prénom : ')
+age = int(input('Entrez votre age : '))
+ville = input('Entrez votre ville : ')
+metier = input('Entrez votre metier : ')
+
+jours_vecus= age * 365
+
+print('Prenom : ', prenom )
+print(f"Age : {age} ans ({jours_vecus} jours vecus environ")
